48a8bb4b1cb122727e2c1c67078b1a989732fdf0
kimi5231 <djwjod@naver.com>
2026-03-17 15:31:16 +0900

    2주차 실습

---
+++ Client/Client/Main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <WS2tcpip.h>
+
+#pragma comment (lib, "WS2_32.LIB")
+
+using namespace std;
+
+const char* SERVER_ADDR = "127.0.0.1";
+const short SERVER_PORT = 4000;
+const int BUFSIZE = 256;
+
+int main()
+{
+	wcout.imbue(locale("korean"));
+
+	WSAStartup(MAKEWORD(2, 2), nullptr);
+
+	SOCKET s_socket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, 0, 0, 0);
+	SOCKADDR_IN server_addr;
+	ZeroMemory(&server_addr, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(SERVER_PORT);
+	inet_pton(AF_INET, SERVER_ADDR, &server_addr.sin_addr);
+	WSAConnect(s_socket, reinterpret_cast<SOCKADDR*>(&server_addr), sizeof(server_addr), NULL, NULL, NULL, NULL);
+	
+	for (;;)
+	{
+		char buf[BUFSIZE];
+		cout << "Enter Message : "; cin.getline(buf, BUFSIZE);
+
+		DWORD sent_size;
+		WSABUF was_buf{ strlen(buf + 1), buf};
+		WSASend(234, &was_buf, 1, &sent_size, 0, nullptr, nullptr);
+
+		char recv_buf[BUFSIZE];
+		WSABUF recv_wsa_buf{ BUFSIZE, recv_buf };
+		DWORD recv_size = 0;
+		DWORD recv_flag = 0;
+		WSARecv(s_socket, &recv_wsa_buf, 1, &recv_size, &recv_flag, nullptr, nullptr);
+		
+		cout << "Server Sent [" << recv_size << "bytes] : " << recv_buf << endl;
+	}
+
+	WSACleanup();
+}
---
+++ Server/Server/Main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <WS2tcpip.h>
+
+#pragma comment (lib, "WS2_32.LIB")
+
+using namespace std;
+
+const short SERVER_PORT = 4000;
+const int BUFSIZE = 256;
+
+int main()
+{
+	WSAStartup(MAKEWORD(2, 2), nullptr);
+
+	SOCKET s_socket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, 0, 0, 0);
+	SOCKADDR_IN server_addr;
+	ZeroMemory(&server_addr, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(SERVER_PORT);
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	bind(s_socket, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr));
+	
+	listen(s_socket, SOMAXCONN);
+	
+	INT addr_size = sizeof(server_addr);
+	SOCKET c_socket = WSAAccept(s_socket, reinterpret_cast<sockaddr*>(&server_addr), &addr_size, 0, 0);
+	
+	for (;;) {
+		char recv_buf[BUFSIZE];
+		WSABUF mybuf;
+		mybuf.buf = recv_buf; mybuf.len = BUFSIZE;
+		DWORD recv_byte;
+		DWORD recv_flag = 0;
+		WSARecv(c_socket, &mybuf, 1, &recv_byte, &recv_flag, 0, 0);
+		cout << "Client Sent [" << recv_byte << "bytes] : " << recv_buf << endl;
+		DWORD sent_byte;
+		mybuf.len = recv_byte;
+		WSASend(c_socket, &mybuf, 1, &sent_byte, 0, 0, 0);
+	}
+	
+	WSACleanup();
+}

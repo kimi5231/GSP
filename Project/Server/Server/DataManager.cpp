6f8bde7cddf8d7984c3b0decd131cdb819d6ae52
kimi5231 <djwjod@naver.com>
2026-06-12 19:15:09 +0900

    타일맵 적용 중간 저장

--- Project/Server/Server/DataManager.cpp
+++ Project/Server/Server/DataManager.cpp
@@ -19,34 +19,22 @@ void DataManager::LoadTilemaps()
     json data = json::parse(file);
 
     // Tilemap √ﬂ√‚
-    /*for (const auto& tilemap : data["tilemaps"])
+    Vector tileCount{ data["tileCounts"][0],  data["tileCounts"][1] };
+
+    std::vector<std::vector<short>> tilemap;
+
+    std::vector<short> x;
+    for (const auto& tiles : data["tiles"])
     {
-        CubeType type = tilemap["type"];
-        Vector tileCount{ tilemap["tileCount"][0], tilemap["tileCount"][1], tilemap["tileCount"][2] };
-
-        std::vector<std::vector<std::vector<short>>> tilemap3D;
-        for (const auto& layer : tilemap["tilemap"])
+        for (const auto& pattern : tiles["pattern"])
         {
-            std::vector<std::vector<short>> tilemap2D;
-            for (const auto& matrix : layer["layer"])
-            {
-                std::vector<short> tilemap1D;
-                for (const auto& pattern : matrix["pattern"])
-                {
-                    for (int i = 0; i < pattern[1]; i++)
-                        tilemap1D.push_back(pattern[0]);
-                }
-
-                for (int i = 0; i < matrix["repeatY"]; i++)
-                    tilemap2D.push_back(tilemap1D);
-            }
-
-            for (int i = 0; i < layer["repeatZ"]; i++)
-                tilemap3D.push_back(tilemap2D);
+            for (int i = 0; i < pattern[1]; i++)
+                x.push_back(pattern[0]);
         }
 
-        _cubeTilemaps[type] = tilemap3D;
-    }*/
+        for (int i = 0; i < tiles["repeatY"]; i++)
+            tilemap.push_back(x);
+    }
 }
 
 void DataManager::LoadItemInfos()
